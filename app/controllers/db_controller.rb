d6f33f942675df5a2f0593947f940241d8e58105
Pawan Patel <patelpawan1996@hotmail.com>
2019-11-27 17:08:21 -0500

    fixed api

--- app/controllers/db_controller.rb
+++ app/controllers/db_controller.rb
@@ -1,7 +1,9 @@
 class DbController<ApplicationController
 
   def home
-  response = Excon.get(     "https://api.openweathermap.org/data/2.5/weather?q=oshawa&appid=b81347c5672e23b4e026a7533d499673",    )   return nil if response.status != 200   @weather = JSON.parse(response.body)
+  response = Excon.get("https://api.openweathermap.org/data/2.5/weather?q=oshawa&appid=b81347c5672e23b4e026a7533d499673")
+   return nil if response.status != 200
+   @weather = JSON.parse(response.body)
   end
 
 
